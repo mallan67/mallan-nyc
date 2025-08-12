733983124490e3ad4adaa7cb9e49bdea9b48309f
mallan67 <mayad67@gmail.com>
2025-08-12 06:54:23 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
-import type { NextRequest } from 'next/server';
-import { NextResponse } from 'next/server';
-
-export function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === '/api/acris') {
-    const url = req.nextUrl.clone();
-    url.pathname = '/api/acris2';
-    return NextResponse.rewrite(url);
-  }
-  return NextResponse.next();
-}
-export const config = { matcher: ['/api/acris'] };
+export const config = {
+  matcher: [
+    // Run middleware on everything EXCEPT these:
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)',
+  ],
+};
