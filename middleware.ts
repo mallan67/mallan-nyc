49ff4cdf4561d3940da61054153ba4fcedb13aba
mallan67 <mayad67@gmail.com>
2025-08-12 05:56:27 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
-// Only rewrite /api/acris → /api/acris2
 export function middleware(req: NextRequest) {
   if (req.nextUrl.pathname === '/api/acris') {
     const url = req.nextUrl.clone();
@@ -10,8 +9,4 @@ export function middleware(req: NextRequest) {
   }
   return NextResponse.next();
 }
-
-// Limit middleware to that single path
-export const config = {
-  matcher: ['/api/acris'],
-};
+export const config = { matcher: ['/api/acris'] };
