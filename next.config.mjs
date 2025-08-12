0fcca7a303bca3716db5ef4a29791e1b6321ca27
mallan67 <mayad67@gmail.com>
2025-08-12 05:37:01 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
-    return [
-      // Send /api/acris to our working handler
-      { source: '/api/acris', destination: '/api/acris2' },
-    ];
+    return [{ source: '/api/acris', destination: '/api/acris2' }];
   },
 };
-
 export default nextConfig;
