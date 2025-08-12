2acb9090176b4f83c002cb7c4bebec1040802ed5
mallan67 <mayad67@gmail.com>
2025-08-12 07:20:36 -0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [{ source: '/api/acris', destination: '/api/acris2' }];
-  },
-};
-export default nextConfig;
