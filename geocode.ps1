3262db15915931e6f8efd94cca0a60742816219c
mallan67 <mayad67@gmail.com>
2025-09-26 17:42:16 -0400

    Update geocode.ps1

--- geocode.ps1
+++ geocode.ps1
@@ -2,6 +2,7 @@ param(
   [string]$In  = "addresses.csv",
   [string]$Out = "geoclient_addresses_out.csv"
 )
+$ErrorActionPreference = 'Stop'
 
 if (-not (Test-Path $In)) { throw "Input CSV not found: $In" }
 if (-not $env:NYC_GEOCLIENT_KEY) { throw "NYC_GEOCLIENT_KEY environment variable is missing." }
@@ -9,7 +10,9 @@ if (-not $env:NYC_GEOCLIENT_KEY) { throw "NYC_GEOCLIENT_KEY environment variable
 function Invoke-NycGeoClient {
   param([hashtable]$q)
   $base = 'https://api.nyc.gov/geoclient/v2/address'
-  $qs   = ($q.GetEnumerator() | ForEach-Object { '{0}={1}' -f [uri]::EscapeDataString($_.Key), [uri]::EscapeDataString([string]$_.Value) }) -join '&'
+  $qs   = ($q.GetEnumerator() | ForEach-Object {
+      '{0}={1}' -f [uri]::EscapeDataString($_.Key), [uri]::EscapeDataString([string]$_.Value)
+  }) -join '&'
   $uri  = "$base?$qs"
   $h    = @{ 'Ocp-Apim-Subscription-Key' = $env:NYC_GEOCLIENT_KEY }
   (Invoke-RestMethod -Headers $h -Uri $uri -ErrorAction Stop).address
@@ -22,13 +25,12 @@ Import-Csv $In | ForEach-Object {
     borough     = $_.borough
     zip         = $_.zip
   }
-
   [pscustomobject]@{
-    BBL        = $addr.bbl
-    BIN        = $addr.buildingIdentificationNumber
-    Borough    = $addr.firstBoroughName
-    Latitude   = $addr.latitude
-    Longitude  = $addr.longitude
-    Precinct   = $addr.policePrecinct
+    BBL       = $addr.bbl
+    BIN       = $addr.buildingIdentificationNumber
+    Borough   = $addr.firstBoroughName
+    Latitude  = $addr.latitude
+    Longitude = $addr.longitude
+    Precinct  = $addr.policePrecinct
   }
 } | Export-Csv $Out -NoTypeInformation -Encoding UTF8
