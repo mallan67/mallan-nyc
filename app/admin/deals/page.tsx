eea2bbf82d965728e034d9f1d8395c5c92097ffc
mallan67 <mayad67@gmail.com>
2025-10-31 12:49:27 -0400

    Update page.tsx

--- app/admin/deals/page.tsx
+++ app/admin/deals/page.tsx
@@ -2,10 +2,6 @@
 
 import React, { useEffect, useState } from 'react';
 
-'use client';
-
-import React, { useEffect, useState } from 'react';
-
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 export const fetchCache = 'force-no-store';
